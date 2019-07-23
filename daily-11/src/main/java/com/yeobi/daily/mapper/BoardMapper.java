100ae17854fbbd2876f10b5187b0f9991448d521
lemon <lemon@DESKTOP-NQOOPGA>
2019-07-24 01:07:36 +0900

    gogosing2

--- daily-11/src/main/java/com/yeobi/daily/mapper/BoardMapper.java
+++ daily-11/src/main/java/com/yeobi/daily/mapper/BoardMapper.java
@@ -7,9 +7,20 @@ import com.yeobi.daily.board.BoardVO;
 public interface BoardMapper {
 	 
 	//글작성
-    public void boardInsert(BoardVO board)throws Exception;
- 
-    //글목록
-    public List<BoardVO> boardList()throws Exception; //추가
+    public void boardInsert(BoardVO vo)throws Exception;
     
+    //글목록
+    public List<BoardVO> boardList()throws Exception;
+    
+    //글보기
+    public BoardVO boardView(int bno)throws Exception;
+    
+    //조회수 증가
+    public void hitPlus(int bno)throws Exception;
+    
+    //글수정
+    public void boardUpdate(BoardVO vo)throws Exception;
+    
+    //글삭제
+    public void boardDelete(int bno)throws Exception;
 }
